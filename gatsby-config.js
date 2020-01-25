0d214b2b3b167d3ddb1441480fea417f4eeca5e3
Pascal Ernst <pascalernst@MacBook-Pro-16-Pascal-Ernst.local>
2020-01-25 20:25:02 +0000

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,82 @@
+module.exports = {
+    siteMetadata: {
+        title: `LinuCC's Blog`,
+        author: `Pascal Ernst`,
+        description: `So much to discover. So little time.`,
+        siteUrl: `https://linu.cc`,
+        social: {
+            // twitter: `kylemathews`,
+            twitter: ``,
+        },
+    },
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/blog`,
+                name: `blog`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/assets`,
+                name: `assets`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
+                    `gatsby-remark-prismjs`,
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`,
+                ],
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-sass`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                //trackingId: `ADD YOUR TRACKING ID HERE`,
+            },
+        },
+        `gatsby-plugin-feed`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `LinuCC's Blog`,
+                short_name: `LinuCC`,
+                start_url: `/`,
+                background_color: `#2e3440`,
+                theme_color: `#88c0d0`,
+                display: `minimal-ui`,
+                icon: `content/assets/d96512x512.png`,
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`,
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+}
